fb9f791777f07796278227fd4f8b16b4fc8ff243
Edgar Otaqui Cabrera <edgarotaquicabrera@macbook-pro-de-edgar-otaqui-cabrera.local>
2016-11-08 19:25:20 -0600

    Initial Commit

---
+++ pruebagithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebagithub
+//
+//  Created by Edgar Otaqui Cabrera on 08/11/16.
+//  Copyright © 2016 Edgar Otaqui Cabrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
